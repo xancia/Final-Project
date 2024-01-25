4d7cb495537908719ef6e43960b545f6f0d6d8e8
Xancia <cminhma@gmail.com>
2024-01-25 16:12:02 -0500

    formatting

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,7 +10,6 @@ function generateToken(newUser) {
 async function register(req, res) {
     console.log('REGISTER /auth/register')
     try {
-        // 1. Check if the user exists 
 
         const foundUser = await User.findOne({ username: req.body.username })
 
@@ -18,25 +17,14 @@ async function register(req, res) {
             return res.status(400).json({ error: 'User already exists' })
         }
 
-        // 2. If they don't (new user... good!) encrypt the password
 
         const encryptedPassword = await bcrypt.hash(req.body.password, 10)
 
-        console.log(encryptedPassword)
-
-        // 3. Add new user to the database (with the encrypted password)
-
-        console.log({ ...req.body, password: encryptedPassword })
 
         const newUser = await User.create({ ...req.body, password: encryptedPassword })
 
-        console.log(newUser)
-
-        // 4. Generate a JWT token (the keys... permission slip... wrist band) and returning it to the user 
             
         const token = generateToken(newUser)
-
-        console.log(token)
 
         res.status(200).json({ token })
 
@@ -50,7 +38,7 @@ async function register(req, res) {
 async function login(req, res) {
     console.log('LOGIN /auth/login')
     try {
-        // 1. Check if user exists
+
 
         const foundUser = await User.findOne({ username: req.body.username })
 
@@ -58,7 +46,6 @@ async function login(req, res) {
             return res.status(400).json({ error: 'No such user exists' }) 
         }
 
-        // 2. Check if the password provided by user matches the one in the database
 
         const validPass = await bcrypt.compare(req.body.password, foundUser.password)
 
@@ -66,7 +53,6 @@ async function login(req, res) {
             return res.status(400).json({ error: 'Invalid credentials' })
         }
 
-        // 3. Generate a JWT token and return it to the user
 
         const token = generateToken(foundUser)
         
