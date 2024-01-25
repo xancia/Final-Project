b68e7e6dd64a430401c0a9693bfc2976960a1cce
Xancia <cminhma@gmail.com>
2024-01-25 16:13:10 -0500

    formatting

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,45 +1,33 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 function authorize(req, res, next) {
+  try {
+    console.log("authorizing...");
 
-    try {
-        console.log('authorizing...')
+    let token = req.header("Authorization");
 
-        // 1. Check if the request has a token (in the Authorization header)
-
-        let token = req.header("Authorization")
-
-        if (!token) {
-            return res.status(400).json({ error: 'No token provided' })
-        }
-
-        console.log(token) // "Bearer 9js83485jsh834"
-
-        token = token.replace("Bearer ", "") // "9js83485jsh834"
-
-        // 2. Check that the token is valid and not expired
-
-        const payload = jwt.verify(token, process.env.JWT_SECRET)
-
-        if (payload.error) {
-            return res.status(400).json({ error: payload.error })
-        }
-
-        // 3. Attach the payload from the token to the request object (req)
-
-        req.id = payload.id
-        req.username = payload.username
-
-        // 4. Move on to the requested route (next)
-
-        next()
-
-    } catch(err) {
-        console.log(err.message)
-        res.status(400).json({ error: err.message })
+    if (!token) {
+      return res.status(400).json({ error: "No token provided" });
     }
+
+    token = token.replace("Bearer ", ""); // "9js83485jsh834"
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (payload.error) {
+      return res.status(400).json({ error: payload.error });
+    }
+
+    req.id = payload.id;
+    req.username = payload.username;
+
+    next();
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json({ error: err.message });
+  }
 }
 
 module.exports = {
-    authorize
-}
+  authorize,
+};
