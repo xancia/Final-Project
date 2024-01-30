802603e062a24c9afaa7ef8598392b056a4fd689
Xancia <cminhma@gmail.com>
2024-01-30 12:00:29 -0500

    built out the anime page

--- client/src/components/pages/AnimePage.tsx
+++ client/src/components/pages/AnimePage.tsx
@@ -1,53 +1,251 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useParams } from "react-router-dom";
-import {Jakan, JakanData} from "jakan";
+import { Jakan } from "jakan";
 import { useEffect, useState } from "react";
 import { NavBar } from "../NavBar";
 import Container from "../utility/Container";
-import { ExtendedAnimeData } from "@/vite-env";
+import { ExtendedAnimeData, userType } from "@/vite-env";
 import { Icon } from "@iconify/react/dist/iconify.js";
-
-
-
+import { RootState } from "../utility/store";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { addSavedAnime, removeSavedAnime } from "../utility/userDataSlice";
+import { Button } from "../ui/button";
 
 const AnimePage = () => {
-    const {id} = useParams();
-    const animeID = Number(id)
-    const jakan = new Jakan().withMemory().forSearch()
-    const [anime, setAnime] = useState<ExtendedAnimeData | null>(null)
-    console.log(id)
+  const { id } = useParams();
+  const animeID = Number(id);
+  const jakan = new Jakan().withMemory().forSearch();
+  const [anime, setAnime] = useState<ExtendedAnimeData | null>(null);
+  const userData = useSelector(
+    (state: RootState) => state.userData as userType | null
+  );
+  const isAnimeSaved = userData?.animeList?.some(
+    (item) => item.mal_id === anime?.mal_id
+  );
+  const dispatch = useDispatch();
 
-    async function getAnime() {
-        const animeData = await jakan.anime(animeID)
-        setAnime(animeData.data)
+  async function getAnime() {
+    const animeData = await jakan.anime(animeID);
+    setAnime(animeData.data);
+  }
+
+  useEffect(() => {
+    getAnime();
+  }, []);
+
+  function convertDateString(dateStr: any) {
+    const date = new Date(dateStr);
+    return date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+  }
+
+  const premiereDate = convertDateString(anime?.aired?.from);
+
+  async function handleClick() {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) throw new Error("No token found");
+
+      // Preparing the payload
+      const animeData = {
+        mal_id: anime?.mal_id,
+        image: anime?.images?.jpg.image_url,
+        title: anime?.title,
+        broadcastTime: anime?.broadcast?.time,
+        broadcastTimeZone: anime?.broadcast?.timezone,
+        score: anime?.score,
+      };
+
+      // Sending a POST request to the server
+      const response = await axios.post(
+        "http://localhost:8080/api/users/anime",
+        animeData,
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      dispatch(addSavedAnime(animeData));
+      console.log("Anime added:", response.data);
+    } catch (error) {
+      console.error("Error adding anime:", error);
     }
+  }
 
-    useEffect(() => {
-        getAnime()
-    }, [])
-    
-    console.log(anime)
+  async function handleRemove() {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) throw new Error("No token found");
 
+      // Preparing the payload
+      const animeData = { mal_id: anime?.mal_id };
+
+      // Sending a DELETE request to the server
+      await axios.delete("http://localhost:8080/api/users/anime", {
+        headers: {
+          Authorization: token,
+        },
+        data: animeData, // Axios DELETE sends data in the 'data' field
+      });
+
+      dispatch(removeSavedAnime(anime?.mal_id));
+
+      console.log("Anime removed:", anime?.mal_id);
+    } catch (error) {
+      console.error("Error removing anime:", error);
+    }
+  }
+
+  console.log(anime);
 
   return (
     <div>
-        <NavBar />
-        {anime && 
+      <NavBar />
+      {anime && (
         <div className="pt-20">
-            <Container className="flex-col sm:flex-row">
-                <div className="flex flex-col m-4 p-2">
-                    <img src={anime.images?.jpg.image_url} alt="" />
-                    <div className="flex items-center py-4 px-2">
-                        <Icon className="" icon='ic:baseline-star-rate'/>
-                        <p className="text-xl pl-2 font-bold pb-1">{anime.score}</p><p className="text-slate-400">/10</p>
-                    </div>
-                    <div>{anime.scored_by}</div>
+          <Container className="flex-col sm:flex-row">
+            <div className="flex flex-col m-4 p-2">
+              <img
+                src={anime.images?.jpg.image_url}
+                alt=""
+                className="rounded-sm"
+              />
+              <div className="flex items-center pt-4 px-2">
+                <Icon className="" icon="ic:baseline-star-rate" />
+                <div className="px-2 flex flex-col">
+                  <div className="flex flex-row items-center">
+                    <p className="text-xl pl-2 font-bold">{anime.score}</p>
+                    <p className="text-slate-400 mt-1">/10</p>
+                  </div>
+                  <p className="pl-2">{anime.scored_by} ratings</p>
                 </div>
-                <div className="flex flex-col m-4 p-2">2</div>
-            </Container>
-        </div>}
-    </div>
-  )
-}
+              </div>
+              <div className="flex flex-col py-4">
+                <p className="font-bold">Original Title</p>
+                <p>{anime.title_japanese}</p>
+              </div>
+              <div className="flex flex-col py-4">
+                <p className="font-bold">Status</p>
+                {anime.airing ? (
+                  <p>Currently Airing</p>
+                ) : (
+                  <p>Finished Airing</p>
+                )}
+              </div>
+              <div className="flex flex-col py-4">
+                <p className="font-bold">Premiere</p>
+                <p>{premiereDate}</p>
+              </div>
+              <div className="flex flex-col py-4">
+                <p className="font-bold">Season</p>
+                <p>
+                  {anime.season} {anime.year}
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col m-4 p-2 w-full">
+              <div className="flex justify-between">
+                <div>
+                  <p className="font-bold text-2xl">{anime.title}</p>
+                  <p className="font-xl">{anime.title_english}</p>
+                </div>
 
-export default AnimePage
+                <div className="flex items-center">
+                  {isAnimeSaved ? (
+                    <button className="" onClick={handleRemove}>
+                      <Icon
+                        className="text-4xl"
+                        icon="material-symbols:bookmark-sharp"
+                      />
+                    </button>
+                  ) : (
+                    <button className="" onClick={handleClick}>
+                      <Icon
+                        className="text-4xl"
+                        icon="material-symbols:bookmark-outline-sharp"
+                      />
+                    </button>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col bg-slate-900 rounded-md mt-10">
+                <div className="flex flex-row justify-evenly p-4">
+                  <div>
+                    <div className="flex flex-col items-center">
+                      <p className="text-sm text-slate-400">Format</p>
+                      <p className="font-bold">{anime.type}</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="flex flex-col items-center">
+                      <p className="text-sm text-slate-400">Source</p>
+                      <p className="font-bold">{anime.source}</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="flex flex-col items-center">
+                      <p className="text-sm text-slate-400">Episodes</p>
+                      <p className="font-bold">{anime.episodes}</p>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="flex flex-col items-center">
+                      <p className="text-sm text-slate-400">Run time</p>
+                      <p className="font-bold">{anime.duration}</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col items-center">
+                  <p className="text-2xl font-bold">Trailer</p>
+                  <iframe
+                    width="560"
+                    height="315"
+                    src={anime.trailer?.embed_url}
+                    allow="encrypted-media"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+                <div className="p-4">
+                  <p className="py-2 font-bold">Studio</p>
+                  {anime.studios?.map((studio) => (
+                    <Button key={studio.mal_id} className="rounded-lg" asChild>
+                      <a
+                        href={studio.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {studio.name}
+                      </a>
+                    </Button>
+                  ))}
+                </div>
+                <div className="p-4">
+                  <p className="py-2 font-bold">Tags</p>
+                  {anime.genres?.map((genre) => (
+                    <Button key={genre.mal_id} className="rounded-lg mr-2" asChild>
+                      <a
+                        href={genre.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {genre.name}
+                      </a>
+                    </Button>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </Container>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AnimePage;
--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -7,109 +7,105 @@ export type animeIDType = {
 }
 
 export interface ExtendedAnimeData extends JakanData {
-    mal_id: number;
-    url: string;
-    images: {
+    url?: string;
+    images?: {
         jpg: {
-            image_url: string;
-            small_image_url: string;
-            large_image_url: string;
+            image_url?: string;
+            small_image_url?: string;
+            large_image_url?: string;
         };
         webp: {
-            image_url: string;
-            small_image_url: string;
-            large_image_url: string;
+            image_url?: string;
+            small_image_url?: string;
+            large_image_url?: string;
         };
     };
-    trailer: {
-        youtube_id: string;
-        url: string;
-        embed_url: string;
-        images: {
-            image_url: string;
-            small_image_url: string;
-            medium_image_url: string;
-            large_image_url: string;
-            maximum_image_url: string;
+    trailer?: {
+        youtube_id?: string;
+        url?: string;
+        embed_url?: string;
+        images?: {
+            image_url?: string;
+            small_image_url?: string;
+            medium_image_url?: string;
+            large_image_url?: string;
+            maximum_image_url?: string;
         };
     };
-    approved: boolean;
-    titles: {
-        type: string;
-        title: string;
-    }[];
-    title: string;
-    title_english: string;
-    title_japanese: string;
-    title_synonyms: string[];
-    type: string;
-    source: string;
-    episodes: number;
-    status: string;
-    airing: boolean;
-    aired: {
-        from: string;
-        to: string | null;
-        prop: {
-            from: {
-                day: number;
-                month: number;
-                year: number;
+    approved?: boolean;
+    title?: string;
+    title_english?: string;
+    title_japanese?: string;
+    title_synonyms?: string[];
+    type?: string;
+    source?: string;
+    episodes?: number;
+    status?: string;
+    airing?: boolean;
+    aired?: {
+        from?: string;
+        to?: string | null;
+        prop?: {
+            from?: {
+                day?: number;
+                month?: number;
+                year?: number;
             };
-            to: {
-                day: number | null;
-                month: number | null;
-                year: number | null;
+            to?: {
+                day?: number | null;
+                month?: number | null;
+                year?: number | null;
             };
         };
-        string: string;
+        string?: string;
     };
-    duration: string;
-    rating: string;
-    score: number;
-    scored_by: number;
-    rank: number;
-    popularity: number;
-    members: number;
-    favorites: number;
-    synopsis: string;
-    background: string | null;
-    season: string;
-    year: number;
+    duration?: string;
+    rating?: string;
+    score?: number;
+    scored_by?: number;
+    rank?: number;
+    popularity?: number;
+    members?: number;
+    favorites?: number;
+    synopsis?: string;
+    background?: string | null;
+    season?: string;
+    year?: number;
     broadcast?: {
-        day: string;
-        time: string;
-        timezone: string;
-        string: string;
+        day?: string;
+        time?: string;
+        timezone?: string;
+        string?: string;
     };
-    producers: {
-        mal_id: number;
-        type: string;
-        name: string;
-        url: string;
+    producers?: {
+        mal_id?: number;
+        type?: string;
+        name?: string;
+        url?: string;
     }[];
-    licensors: any[]; 
-    studios: {
-        mal_id: number;
-        type: string;
-        name: string;
-        url: string;
+    licensors?: any[];  
+    studios?: {
+        mal_id?: number;
+        type?: string;
+        name?: string;
+        url?: string;
     }[];
-    genres: {
-        mal_id: number;
-        type: string;
-        name: string;
-        url: string;
+    genres?: {
+        mal_id?: number;
+        type?: string;
+        name?: string;
+        url?: string;
     }[];
-    explicit_genres: any[]; 
-    themes: {
-        mal_id: number;
-        type: string;
-        name: string;
-        url: string;
+    explicit_genres?: any[]; 
+    themes?: {
+        mal_id?: number;
+        type?: string;
+        name?: string;
+        url?: string;
     }[];
-    demographics: any[]; 
+    demographics?: any[]; 
 }
+
 
 
 export type userType = {
