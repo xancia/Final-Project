5f8f786a0f204b494c333fd2b24b8bb159e6bac7
Xancia <cminhma@gmail.com>
2024-01-31 12:41:33 -0500

    updated query to only allow sfw searches

--- client/src/components/AnimeSearchSheet.tsx
+++ client/src/components/AnimeSearchSheet.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/sheet";
 import { Input } from "./ui/input";
 import { useState } from "react";
-import { Jakan } from "jakan";
+import { AnimeSearchParameters, Jakan } from "jakan";
 import { ExtendedAnimeData } from "@/vite-env";
 import AnimeCard from "./AnimeCard";
 import { ScrollArea } from "./ui/scroll-area";
@@ -17,8 +17,12 @@ const AnimeSearchSheet = () => {
   const jakan = new Jakan().withMemory().forSearch();
   const [input, setinput] = useState("");
   const [anime, setAnime] = useState<ExtendedAnimeData[] | null>(null);
+  const myQuery: AnimeSearchParameters = {
+    q: input,
+    sfw: true
+  }
   async function handleSubmit() {
-    const animeData = await jakan.anime(input);
+    const animeData = await jakan.anime(myQuery);
     setAnime(animeData.data);
   }
   return (
